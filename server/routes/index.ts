ef3fd588d9bb647a4b482b6e167869eef32cc48a
denis-davidov-moj <151540647+denis-davidov-moj@users.noreply.github.com>
2024-04-13 07:55:38 +0100

    adding hello world (#4)

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -5,15 +5,19 @@ import type { Services } from '../services'
 import { Page } from '../services/auditService'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function routes({ auditService }: Services): Router {
+export default function routes({ auditService, helloWorldService }: Services): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/', async (req, res, next) => {
     await auditService.logPageView(Page.EXAMPLE_PAGE, { who: res.locals.user?.username, correlationId: req.id })
-
     res.render('pages/index')
   })
 
+  get('/hello/:q', async (req, res, next) => {
+    const { q } = req.params
+    const response = await helloWorldService.helloWorld(q)
+    res.render('pages/hello-world', { q, response })
+  })
   return router
 }
--- server/services/index.ts
+++ server/services/index.ts
@@ -1,17 +1,19 @@
 import { dataAccess } from '../data'
 import AuditService from './auditService'
 import UserService from './userService'
+import HelloWorldService from './helloWorld'
 
 export const services = () => {
   const { applicationInfo, manageUsersApiClient, hmppsAuditClient } = dataAccess()
 
   const userService = new UserService(manageUsersApiClient)
   const auditService = new AuditService(hmppsAuditClient)
-
+  const helloWorldService = new HelloWorldService()
   return {
     applicationInfo,
     userService,
     auditService,
+    helloWorldService,
   }
 }
 
