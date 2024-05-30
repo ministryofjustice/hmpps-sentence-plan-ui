1f589168b059425daf1ea6bdc57141672789a9cb
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-05-30 16:35:00 +0100

    Locale service

--- server/utils/utils.ts
+++ server/utils/utils.ts
@@ -50,3 +50,20 @@ export function formatDate(date: string): string {
     year: 'numeric',
   })
 }
+
+export function deepMerge(target: Record<string, any>, source: Record<string, any>): Record<string, any> {
+  for (const key in source) {
+    if (source.hasOwnProperty(key)) {
+      if (typeof source[key] === 'object' && !Array.isArray(source[key])) {
+        if (!target[key] || typeof target[key] !== 'object') {
+          target[key] = {};
+        }
+        target[key] = deepMerge(target[key], source[key]);
+      } else {
+        target[key] = source[key];
+      }
+    }
+  }
+
+  return target;
+}
