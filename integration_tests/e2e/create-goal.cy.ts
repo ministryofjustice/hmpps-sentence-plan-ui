e26998df3f041a8dde202b995c3f040c1bfbf526
Phil Wilson <github@philwilson.org>
2024-07-10 10:36:24 +0100

    Add OASTUB env var and read it

--- integration_tests/e2e/create-goal.cy.ts
+++ integration_tests/e2e/create-goal.cy.ts
@@ -3,7 +3,7 @@ const login = () => {
     cy.visit(Cypress.env('oaStubUrl'))
     cy.get('#target-service').select('sentence-plan')
     cy.get('button').contains('Create handover link').click()
-    cy.location('href').should('eq', 'http://localhost:7072/')
+    cy.location('href').should('eq', Cypress.env('oaStubUrl'))
     cy.get('body > div a[role=button]').contains('Open').invoke('removeAttr', 'target').click()
     cy.location('href').should('eq', 'http://localhost:3000/about-pop')
   })
