4bbb90c12b409fd282b778651d643da44d7d2ba2
Pankaj Khare <pankaj.khare@digital.justice.gov.uk>
2024-07-16 11:08:54 +0100

    display poc

--- server/routes/createGoal/models/CreateGoalPostModel.ts
+++ server/routes/createGoal/models/CreateGoalPostModel.ts
@@ -3,25 +3,24 @@ import { Expose, Transform } from 'class-transformer'
 
 export default class CreateGoalPostModel {
   @IsNotEmpty()
+  'input-autocomplete': string
+
+  @IsNotEmpty()
   'other-area-of-need-radio': string
 
-  'date-selection-custom-day': string
+  @ValidateIf(o => o['other-area-of-need-radio'] === 'yes')
+  @IsNotEmpty()
+  'other-area-of-need': string[]
 
-  'date-selection-custom-month': string
-
-  'date-selection-custom-year': string
+  @IsNotEmpty()
+  'start-working-goal-radio': string
 
   @ValidateIf(o => o['date-selection-radio'] === 'custom')
   @MinDate(new Date())
   @IsDate()
   @Expose()
   @Transform(({ obj }) => {
-    const {
-      'date-selection-custom-year': year,
-      'date-selection-custom-month': month,
-      'date-selection-custom-day': day,
-    } = obj
-    return year && month && day ? new Date(year, month - 1, day) : false
+    return new Date(obj['date-selection-custom'])
   })
   'date-selection-custom': Date
 }
