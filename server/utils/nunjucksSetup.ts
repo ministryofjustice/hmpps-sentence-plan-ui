9e463c826c8ed82d2e7c6993511312192040ec63
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-07-30 15:16:33 +0100

    Fix app title

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -12,7 +12,7 @@ export default function nunjucksSetup(app: express.Express, applicationInfo: App
   app.set('view engine', 'njk')
 
   app.locals.asset_path = '/assets/'
-  app.locals.applicationName = 'Hmpps Sentence Plan Ui'
+  app.locals.applicationName = 'Sentence Plan'
   app.locals.environmentName = config.environmentName
   app.locals.environmentNameColour = config.environmentName === 'PRE-PRODUCTION' ? 'govuk-tag--green' : ''
 
