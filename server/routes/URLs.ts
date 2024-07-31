c0d25cd3664afdeb82a5a0c6db7deb7333cc2651
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-07-31 19:57:17 +0100

    Remove confirm goal controller, routes, view

--- server/routes/URLs.ts
+++ server/routes/URLs.ts
@@ -1,7 +1,6 @@
 export default {
   ABOUT_POP: '/about-pop',
   CREATE_GOAL: '/create-goal/:areaOfNeed',
-  CONFIRM_GOAL: '/confirm-goal',
   CREATE_STEP: '/steps/create',
   PLAN_SUMMARY: '/plan-summary',
   GOAL_DETAIL: '/goal/:uuid',
