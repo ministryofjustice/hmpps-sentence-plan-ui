ef52f8a239f4af0d9bee2b385b3d3f32c94e5a7f
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-07-31 10:07:47 +0100

    lint

--- server/services/sentence-plan/goalService.ts
+++ server/services/sentence-plan/goalService.ts
@@ -1,7 +1,6 @@
 import SentencePlanApiClient from '../../data/sentencePlanApiClient'
 import { NewGoal } from '../../@types/NewGoalType'
 import { Goal } from '../../@types/GoalType'
-import { Step } from '../../@types/StepType'
 import { Goals } from '../../@types/GoalsType'
 
 export default class GoalService {
