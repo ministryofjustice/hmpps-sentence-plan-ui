d1ff0ff53f093e09a0214766a12fc4559b335df9
Pankaj Khare <pankaj.khare@digital.justice.gov.uk>
2024-08-14 13:31:07 +0100

    add step page added

--- server/routes/steps/StepsController.ts
+++ server/routes/steps/StepsController.ts
@@ -4,7 +4,6 @@ import locale from './locale.json'
 import { FORMS } from '../../services/formStorageService'
 import { NewStep } from '../../@types/NewStepType'
 import URLs from '../URLs'
-import options from './options'
 import { toKebabCase } from '../../utils/utils'
 
 export default class StepsController {
@@ -13,11 +12,14 @@ export default class StepsController {
   get = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { errors } = req
-      const result: Record<string, any> = req.services.formStorageService.getFormData(FORMS.CREATE_GOAL)
+      const goalResult: Record<string, any> = req.services.formStorageService.getFormData(FORMS.CREATE_GOAL)
       const {
         processed: { areaOfNeed, title: goal },
-      } = result
+      } = goalResult
       const popData = await req.services.sessionService.getSubjectDetails()
+      const stepResult: Record<string, any> = req.services.formStorageService.getFormData(FORMS.CREATE_STEPS)
+      const addedSteps = stepResult ? stepResult.processed : []
+
       res.render('pages/create-step', {
         locale: locale.en,
         data: {
@@ -25,6 +27,7 @@ export default class StepsController {
           areaOfNeed: toKebabCase(areaOfNeed),
           goal,
           form: req.body,
+          addedSteps,
         },
         errors,
       })
@@ -35,32 +38,27 @@ export default class StepsController {
 
   post = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const {
-        raw: { uuid: currentGoal },
-      }: Record<string, any> = req.services.formStorageService.getFormData('currentGoal')
-      const { actor, someOneElse } = req.body
+      // const {
+      //   raw: { uuid: currentGoal },
+      // }: Record<string, any> = req.services.formStorageService.getFormData('currentGoal')
+      const { actor, action } = req.body
       const stepName = req.body['step-input-autocomplete']
-      const popData = await req.services.sessionService.getSubjectDetails()
-      const payloadOptions = [...options]
-      payloadOptions.unshift({ value: 1, text: popData.givenName })
-      if (someOneElse) payloadOptions[5].text = someOneElse
-      const actorArray = Array.isArray(actor) ? actor : [actor]
-      const actorNumbers: number[] = actorArray.map((item: string | number): number => Number(item))
-      const mappedActor: Array<any> = payloadOptions.filter(option => actorNumbers.includes(option.value))
-      const actors = transformActor(mappedActor)
-      const payload = { description: stepName, status: '', actor: actors } as NewStep
-      await this.stepService.saveSteps([payload], currentGoal)
-      res.redirect(`${URLs.PLAN_SUMMARY}?status=success`)
+      const newStep = { description: stepName, status: '', actor } as NewStep
+      if (action === 'addAnother') {
+        const result: Record<string, any> = req.services.formStorageService.getFormData(FORMS.CREATE_STEPS)
+        const addedSteps = result ? result.processed : []
+        addedSteps.push(newStep)
+        req.services.formStorageService.saveFormData(FORMS.CREATE_STEPS, {
+          processed: addedSteps,
+          raw: req.body,
+        })
+        res.redirect(`${URLs.CREATE_STEP}`)
+      } else {
+        // await this.stepService.saveSteps([payload], currentGoal)
+        res.redirect(`${URLs.PLAN_SUMMARY}?status=success`)
+      }
     } catch (e) {
       next(e)
     }
   }
-}
-
-function transformActor(mappedActor: Array<any>): Array<any> {
-  const actorArray: Array<any> = []
-  mappedActor.forEach(obj => {
-    actorArray.push({ actor: obj.text, actorOptionId: obj.value })
-  })
-  return actorArray
 }
