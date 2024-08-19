371ab3da573572707c722a0a3215117442c327e7
Phil Wilson <pipwilson@users.noreply.github.com>
2024-08-19 12:52:36 +0100

    SP2-608 Adds Agree Plan page (#102)

--- server/routes/agree-plan/AgreePlanController.test.ts
+++ server/routes/agree-plan/AgreePlanController.test.ts
@@ -1,15 +1,24 @@
 import { NextFunction, Request, Response } from 'express'
 import handoverData from '../../testutils/data/handoverData'
+import testPlan from '../../testutils/data/planData'
 import AgreePlanController from './AgreePlanController'
 import PlanService from '../../services/sentence-plan/planService'
 import mockReq from '../../testutils/preMadeMocks/mockReq'
 import mockRes from '../../testutils/preMadeMocks/mockRes'
+import validate from '../../middleware/validationMiddleware'
 import locale from './locale.json'
+import AgreePlanPostModel from './models/AgreePlanPostModel'
 
 jest.mock('../../services/sessionService', () => {
   return jest.fn().mockImplementation(() => ({
     getSubjectDetails: jest.fn().mockReturnValue(handoverData.subject),
     getPlanUUID: jest.fn().mockReturnValue('9506fba0-d2c7-4978-b3fc-aefd86821844'),
+  }))
+})
+
+jest.mock('../../services/sentence-plan/planService', () => {
+  return jest.fn().mockImplementation(() => ({
+    agreePlan: jest.fn().mockResolvedValue(testPlan),
   }))
 })
 
@@ -23,6 +32,7 @@ describe('AgreePlanController', () => {
     data: {
       planUuid: '9506fba0-d2c7-4978-b3fc-aefd86821844',
       popData: handoverData.subject,
+      form: {},
     },
     errors: {},
     locale: locale.en,
@@ -43,16 +53,85 @@ describe('AgreePlanController', () => {
       expect(res.render).toHaveBeenCalledWith('pages/agree-plan', viewData)
     })
 
-    it('should render with validation errors', async () => {})
+    it('should render with validation errors', async () => {
+      const errors = {
+        body: { 'agree-plan-radio': { isNotEmpty: true } },
+        params: {},
+        query: {},
+      }
+      req.errors = errors
+      const expectedViewData = {
+        ...viewData,
+        errors,
+      }
+
+      await controller.get(req, res, next)
+
+      expect(res.render).toHaveBeenCalledWith('pages/agree-plan', expectedViewData)
+    })
   })
 
   describe('post', () => {
-    describe('validation', () => {})
+    describe('validation', () => {
+      it('should render the form again if there are validation errors', async () => {
+        const errors = {
+          body: { 'agree-plan-radio': { isNotEmpty: true } },
+          params: {},
+          query: {},
+        }
+        req.errors = errors
+        const expectedViewData = {
+          ...viewData,
+          errors,
+        }
+
+        await controller.post(req as Request, res as Response, next)
+
+        expect(res.render).toHaveBeenCalledWith('pages/agree-plan', expectedViewData)
+        expect(mockPlanService.agreePlan).not.toHaveBeenCalled()
+        expect(res.redirect).not.toHaveBeenCalled()
+        expect(next).not.toHaveBeenCalled()
+      })
+
+      describe('agree-plan-radio', () => {
+        it('should add error if not selected', async () => {
+          validate({ body: AgreePlanPostModel })(req, null, jest.fn)
+
+          expect(req.errors.body).toMatchObject({
+            'agree-plan-radio': { isNotEmpty: true },
+          })
+        })
+
+        it('should add error if no is selected and details are missing', async () => {
+          req.body['agree-plan-radio'] = 'no'
+
+          validate({ body: AgreePlanPostModel })(req, null, jest.fn)
+
+          expect(req.errors.body).toMatchObject({
+            'does-not-agree-details': { isNotEmpty: true },
+          })
+        })
+
+        it('should add error if could-not-answer is selected and details are missing', async () => {
+          req.body['agree-plan-radio'] = 'couldNotAnswer'
+
+          validate({ body: AgreePlanPostModel })(req, null, jest.fn)
+
+          expect(req.errors.body).toMatchObject({
+            'could-not-answer-details': { isNotEmpty: true },
+          })
+        })
+      })
+    })
+
+    describe('save and redirect', () => {
+      it('should save and redirect when there are no validation errors', async () => {
+        // TODO
+      })
+
+      it('should should return error page if there is an error saving the goal', async () => {
+        // TODO
+      })
+    })
   })
-
-  it('should save and redirect when there are no validation errors', async () => {})
-
-  it('should render the form again if there are validation errors', async () => {})
-
-  it('should should return error page if there is an error saving the goal', async () => {})
 })
--- server/routes/agree-plan/models/AgreePlanPostModel.ts
+++ server/routes/agree-plan/models/AgreePlanPostModel.ts
@@ -1,1 +1,14 @@
-export default class AgreePlanPostModel {}
+import { IsNotEmpty, ValidateIf } from 'class-validator'
+
+export default class AgreePlanPostModel {
+  @IsNotEmpty()
+  'agree-plan-radio': string
+
+  @ValidateIf(o => o['agree-plan-radio'] === 'no')
+  @IsNotEmpty()
+  'does-not-agree-details': string
+
+  @ValidateIf(o => o['agree-plan-radio'] === 'couldNotAnswer')
+  @IsNotEmpty()
+  'could-not-answer-details': string
+}
