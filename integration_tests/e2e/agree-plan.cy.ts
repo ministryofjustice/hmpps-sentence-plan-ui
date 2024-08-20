253a16b5cebcff485f41fbe3cf862432ff88bfad
Pankaj <92032149+pk2453@users.noreply.github.com>
2024-08-20 11:09:10 +0100

    SP2-610 add e2e tests for agree plan (#103)

---
+++ integration_tests/e2e/agree-plan.cy.ts
@@ -0,0 +1,74 @@
+// TODO
+// Once the gating of Agree is in place the tests will start failing because the Plan doesn't have any Goals which contain Steps.
+// At this point use DataGenerator to set that up and also create unhappy path tests for 1) plan with no goals and 2) plan with goals but no steps.
+
+describe('Agree plan', () => {
+  beforeEach(() => {
+    cy.createSentencePlan().then(planDetails => {
+      cy.wrap(planDetails).as('plan')
+      cy.openSentencePlan(planDetails.oasysAssessmentPk)
+      cy.get('.govuk-button').contains('Agree plan').click()
+    })
+  })
+
+  describe('Rendering', () => {
+    it('Display agree plan page correctly on load', () => {
+      cy.get('.govuk-heading-l').contains('agree to this plan?')
+      cy.get('.govuk-label').contains('Yes')
+      cy.get('.govuk-label').contains('No')
+      cy.get('.govuk-label').contains('could not answer this question')
+      cy.get('.govuk-label').contains('Add any notes (optional)')
+      cy.get('.govuk-button').contains('Agree plan with')
+    })
+
+    describe('Validation behaviour', () => {
+      it('Display validation error if nothing is selected', () => {
+        cy.get('.govuk-button').click()
+
+        cy.contains('#agree-plan-radio-error', 'Select if they agree to the plan')
+        cy.title().should('contain', 'Error:')
+      })
+      it('Display validation error if No is selected but no details provided', () => {
+        cy.get('#agree-plan-radio-2').click()
+        cy.get('.govuk-button').click()
+
+        cy.contains('#does-not-agree-details-error', 'Enter details about why they do not agree')
+        cy.title().should('contain', 'Error:')
+      })
+      it('Display validation error if Not answered is selected but no details provided', () => {
+        cy.get('#agree-plan-radio-3').click()
+        cy.get('.govuk-button').click()
+
+        cy.contains('#could-not-answer-details-error', 'Enter details about why they cannot answer')
+        cy.title().should('contain', 'Error:')
+      })
+    })
+
+    describe('Submission behaviour', () => {
+      it('Submit successfully when Yes is selected and additional notes provided', () => {
+        cy.get('#agree-plan-radio').click()
+        cy.get('#notes').type('abc')
+
+        cy.get('.govuk-button').click()
+
+        cy.url().should('include', '/plan-summary')
+      })
+      it('Submit successfully when No is selected and additional information provided', () => {
+        cy.get('#agree-plan-radio-2').click()
+        cy.get('#does-not-agree-details').type('abc')
+
+        cy.get('.govuk-button').click()
+
+        cy.url().should('include', '/plan-summary')
+      })
+      it('Submit successfully when "Could not answer this question" is selected and additional information provided', () => {
+        cy.get('#agree-plan-radio-3').click()
+        cy.get('#could-not-answer-details').type('abc')
+
+        cy.get('.govuk-button').click()
+
+        cy.url().should('include', '/plan-summary')
+      })
+    })
+  })
+})
