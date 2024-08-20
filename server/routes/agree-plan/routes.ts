b83c59685e5df79735150de15278677d637c6be2
Thomas-Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-08-20 18:21:09 +0100

    Expose validation logic to make it easier to do certain validation (#106)

--- server/routes/agree-plan/routes.ts
+++ server/routes/agree-plan/routes.ts
@@ -2,12 +2,12 @@ import { Router } from 'express'
 import URLs from '../URLs'
 import AgreePlanController from './AgreePlanController'
 import type { Services } from '../../services'
-import validate from '../../middleware/validationMiddleware'
 import AgreePlanPostModel from './models/AgreePlanPostModel'
+import validateRequest from '../../middleware/validationMiddleware'
 
 export default function setupAgreePlanRoutes(router: Router, { planService }: Services) {
   const controller = new AgreePlanController(planService)
 
   router.get(URLs.AGREE_PLAN, controller.get)
-  router.post(URLs.AGREE_PLAN, validate({ body: AgreePlanPostModel }), controller.post)
+  router.post(URLs.AGREE_PLAN, validateRequest({ body: AgreePlanPostModel }), controller.post)
 }
