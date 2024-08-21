6271aea074c21a2bc4b7fb8d1712b4cd0e21e2d6
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-08-21 13:28:14 +0100

    Add support for nested validations

--- server/middleware/validationMiddleware.ts
+++ server/middleware/validationMiddleware.ts
@@ -1,6 +1,7 @@
 import { ClassConstructor, plainToInstance } from 'class-transformer'
 import { validateSync, ValidationError } from 'class-validator'
-import { Request, Response, NextFunction } from 'express'
+import { NextFunction, Request, Response } from 'express'
+import 'reflect-metadata'
 
 enum DataSources {
   BODY = 'body',
@@ -8,14 +9,31 @@ enum DataSources {
   QUERY = 'query',
 }
 
-function simplifyValidationErrors(errors: ValidationError[]): any {
-  const result: any = {}
-  errors.forEach(error => {
-    result[error.property] = error.constraints
-      ? Object.keys(error.constraints).reduce((acc, key) => ({ ...acc, [key]: true }), {})
-      : {}
-  })
-  return result
+function simplifyValidationErrors(errorsList: ValidationError[]): Record<string, any> {
+  function buildPrettyError(errorsInner: ValidationError[], path: string = ''): Record<string, any> {
+    return errorsInner.reduce((accumulator, error) => {
+      const currentPath = path ? `${path}.${error.property}` : error.property
+
+      const updatedAccumulator = {
+        ...accumulator,
+        ...(error.constraints && {
+          [currentPath]: Object.keys(error.constraints).reduce(
+            (constraintAccumulator, key) => ({ ...constraintAccumulator, [key]: true }),
+            {} as Record<string, boolean>,
+          ),
+        }),
+      }
+
+      if (error.children && error.children.length > 0) {
+        const childErrors = buildPrettyError(error.children, currentPath)
+        return { ...updatedAccumulator, ...childErrors }
+      }
+
+      return updatedAccumulator
+    }, {})
+  }
+
+  return buildPrettyError(errorsList)
 }
 
 export function getValidationErrors<T>(objectToValidate: object, dtoClass: ClassConstructor<T>) {
