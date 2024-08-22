c818a5e6e403be9a6ab2735fa7e14f90cb579607
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-08-22 15:51:54 +0100

    Refactor edit-goal

--- server/routes/editGoal/routes.ts
+++ server/routes/editGoal/routes.ts
@@ -2,12 +2,10 @@ import { Router } from 'express'
 import type { Services } from '../../services'
 import URLs from '../URLs'
 import EditGoalController from './EditGoalController'
-import EditGoalPostModel from './models/EditGoalPostModel'
-import validateRequest from '../../middleware/validationMiddleware'
 
 export default function setupEditGoalRoutes(router: Router, { referentialDataService, goalService }: Services) {
   const controller = new EditGoalController(referentialDataService, goalService)
 
   router.get(URLs.EDIT_GOAL, controller.get)
-  router.post(URLs.EDIT_GOAL, validateRequest({ body: EditGoalPostModel }), controller.post)
+  router.post(URLs.EDIT_GOAL, controller.post)
 }
