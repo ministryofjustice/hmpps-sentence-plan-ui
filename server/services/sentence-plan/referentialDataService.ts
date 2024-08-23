1305e230b1058239fe77f34f2f08c6e4aa4c9737
izzyL-DJ <isaiah.lakanu@digital.justice.gov.uk>
2024-08-23 16:30:53 +0100

    Cleaned up sortedAoN function

--- server/services/sentence-plan/referentialDataService.ts
+++ server/services/sentence-plan/referentialDataService.ts
@@ -7,18 +7,7 @@ export default class ReferentialDataService {
 
   getAreasOfNeed = (): Array<AreaOfNeed> => referenceData.map(({ id, name }) => ({ id, name, url: toKebabCase(name) }))
 
-  getSortedAreasOfNeed = (): Array<AreaOfNeed> =>
-    this.getAreasOfNeed().sort((a, b) => {
-      if (a.name < b.name) {
-        return -1
-      }
-
-      if (a.name > b.name) {
-        return 1
-      }
-
-      return 0
-    })
+  getSortedAreasOfNeed = (): Array<AreaOfNeed> => this.getAreasOfNeed().sort((a, b) => a.name.localeCompare(b.name))
 
   getSteps = (areaOfNeed: string) =>
     referenceData
