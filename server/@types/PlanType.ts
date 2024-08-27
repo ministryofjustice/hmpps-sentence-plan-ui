892cc50e4bb811ac2a8da4ad40384be2155427ff
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-08-27 18:21:33 +0100

    Update PlanType

--- server/@types/PlanType.ts
+++ server/@types/PlanType.ts
@@ -1,6 +1,14 @@
+export enum PlanAgreementStatus {
+  DRAFT = 'DRAFT',
+  AGREED = 'AGREED',
+  DO_NOT_AGREE = 'DO_NOT_AGREE',
+  COULD_NOT_ANSWER = 'COULD_NOT_ANSWER',
+}
+
 export type PlanType = {
   uuid: string
   status: string
+  agreementStatus: PlanAgreementStatus
   creationDate: Date
   updatedDate: Date
 }
