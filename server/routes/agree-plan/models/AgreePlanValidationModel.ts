fda9de1cf840a39b98f5bad340a3c504b4d51a2d
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-08-27 09:31:52 +0100

    Remove unused models

--- server/routes/agree-plan/models/AgreePlanValidationModel.ts
+++
@@ -1,7 +0,0 @@
-import { ArrayNotEmpty, IsNotEmpty, Validate, ValidateIf } from 'class-validator'
-import { GoalReadinessValidator } from './GoalReadinessValidator'
-
-export default class AgreePlanValidationModel {
-  @Validate(GoalReadinessValidator)
-  'now': []
-}
