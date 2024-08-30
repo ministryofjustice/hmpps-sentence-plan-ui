3e866574dd4374e959a1bf04f0401f6c8616474a
Phil Wilson <github@philwilson.org>
2024-08-30 12:02:58 +0100

    Replaces test step creation with DataGenerator and extracts utility selectors from the test

--- integration_tests/e2e/add-steps.cy.ts
+++ integration_tests/e2e/add-steps.cy.ts
@@ -2,16 +2,16 @@ import AddSteps from '../pages/add-steps'
 import { PlanType } from '../../server/@types/PlanType'
 import DataGenerator from '../support/DataGenerator'
 
+const selectStepDescriptionByIndex = (index: number) => {
+  return cy.get(`table.goal-summary-card__steps .govuk-table__body > :nth-child(${index}) > :nth-child(1)`)
+}
+
+const selectStepActorByIndex = (index: number) => {
+  return cy.get(`table.goal-summary-card__steps .govuk-table__body > :nth-child(${index}) > :nth-child(2)`)
+}
+
 describe('Add Steps', () => {
   const addStep = new AddSteps()
-
-  const selectStepDescriptionByIndex = (index: number) => {
-    return cy.get(`table.goal-summary-card__steps .govuk-table__body > :nth-child(${index}) > :nth-child(1)`)
-  }
-
-  const selectStepActorByIndex = (index: number) => {
-    return cy.get(`table.goal-summary-card__steps .govuk-table__body > :nth-child(${index}) > :nth-child(2)`)
-  }
 
   beforeEach(() => {
     cy.createSentencePlan().then(planDetails => {
@@ -32,26 +32,26 @@ describe('Add Steps', () => {
 
     it('Add one step', () => {
       cy.url().should('include', '/add-steps')
-      const stepDescription = 'This is the first step'
-      const stepActor = 'Sam'
-      addStep.addStepAutocompleteText(1, stepDescription)
-      addStep.selectStepActor(1, stepActor)
+
+      const step = DataGenerator.generateStep()
+      addStep.addStepAutocompleteText(1, step.description)
+      addStep.selectStepActor(1, step.actor)
 
       cy.contains('button', 'Remove').should('not.exist')
 
       addStep.saveAndContinue()
 
       cy.get('table.goal-summary-card__steps .govuk-table__body').children().should('have.length', 1)
-      selectStepDescriptionByIndex(1).should('contain', stepDescription)
-      selectStepActorByIndex(1).should('contain', stepActor)
+      selectStepDescriptionByIndex(1).should('contain', step.description)
+      selectStepActorByIndex(1).should('contain', step.actor)
     })
 
     it('Add one step, save, then add another', () => {
       cy.url().should('include', '/add-steps')
-      const firstStepDescription = 'This is the first step'
-      const firstStepActor = 'Sam'
-      addStep.addStepAutocompleteText(1, firstStepDescription)
-      addStep.selectStepActor(1, firstStepActor)
+
+      const firstStep = DataGenerator.generateStep()
+      addStep.addStepAutocompleteText(1, firstStep.description)
+      addStep.selectStepActor(1, firstStep.actor)
 
       addStep.saveAndContinue()
 
@@ -61,40 +61,37 @@ describe('Add Steps', () => {
 
       addStep.addAnotherStepButton()
 
-      const secondStepDescription = 'This is the second step'
-      const secondStepActor = 'Probation practitioner'
-      addStep.addStepAutocompleteText(2, secondStepDescription)
-      addStep.selectStepActor(2, secondStepActor)
+      const secondStep = DataGenerator.generateStep()
+      addStep.addStepAutocompleteText(2, secondStep.description)
+      addStep.selectStepActor(2, secondStep.actor)
 
       addStep.saveAndContinue()
 
       cy.get('table.goal-summary-card__steps .govuk-table__body').children().should('have.length', 2)
-      selectStepDescriptionByIndex(1).should('contain', firstStepDescription)
-      selectStepActorByIndex(1).should('contain', firstStepActor)
-      selectStepDescriptionByIndex(2).should('contain', secondStepDescription)
-      selectStepActorByIndex(2).should('contain', secondStepActor)
+      selectStepDescriptionByIndex(1).should('contain', firstStep.description)
+      selectStepActorByIndex(1).should('contain', firstStep.actor)
+      selectStepDescriptionByIndex(2).should('contain', secondStep.description)
+      selectStepActorByIndex(2).should('contain', secondStep.actor)
     })
 
     it('Add multiple steps, removing one during creation', () => {
       cy.url().should('include', '/add-steps')
-      const firstStepDescription = 'This is the first step'
-      const firstStepActor = 'Sam'
-      addStep.addStepAutocompleteText(1, firstStepDescription)
-      addStep.selectStepActor(1, firstStepActor)
+
+      const firstStep = DataGenerator.generateStep()
+      addStep.addStepAutocompleteText(1, firstStep.description)
+      addStep.selectStepActor(1, firstStep.actor)
 
       addStep.addAnotherStepButton()
 
-      const secondStepDescription = 'This is the second step'
-      const secondStepActor = 'Probation practitioner'
-      addStep.addStepAutocompleteText(2, secondStepDescription)
-      addStep.selectStepActor(2, secondStepActor)
+      const secondStep = DataGenerator.generateStep()
+      addStep.addStepAutocompleteText(2, secondStep.description)
+      addStep.selectStepActor(2, secondStep.actor)
 
       addStep.addAnotherStepButton()
 
-      const thirdStepDescription = 'This is the second step'
-      const thirdStepActor = 'Probation practitioner'
-      addStep.addStepAutocompleteText(3, thirdStepDescription)
-      addStep.selectStepActor(3, thirdStepActor)
+      const thirdStep = DataGenerator.generateStep()
+      addStep.addStepAutocompleteText(3, thirdStep.description)
+      addStep.selectStepActor(3, thirdStep.actor)
 
       cy.get('button[value^="remove-step-"]').should('have.length', 2)
 
@@ -104,26 +101,25 @@ describe('Add Steps', () => {
 
       cy.get('table.goal-summary-card__steps .govuk-table__body').children().should('have.length', 2)
 
-      selectStepDescriptionByIndex(1).should('contain', firstStepDescription)
-      selectStepActorByIndex(1).should('contain', firstStepActor)
+      selectStepDescriptionByIndex(1).should('contain', firstStep.description)
+      selectStepActorByIndex(1).should('contain', firstStep.actor)
 
-      selectStepDescriptionByIndex(2).should('contain', secondStepDescription)
-      selectStepActorByIndex(2).should('contain', secondStepActor)
+      selectStepDescriptionByIndex(2).should('contain', secondStep.description)
+      selectStepActorByIndex(2).should('contain', secondStep.actor)
     })
 
     it('Add multiple steps, removing one afterwards', () => {
       cy.url().should('include', '/add-steps')
-      const firstStepDescription = 'This is the first step'
-      const firstStepActor = 'Sam'
-      addStep.addStepAutocompleteText(1, firstStepDescription)
-      addStep.selectStepActor(1, firstStepActor)
+
+      const firstStep = DataGenerator.generateStep()
+      addStep.addStepAutocompleteText(1, firstStep.description)
+      addStep.selectStepActor(1, firstStep.actor)
 
       addStep.addAnotherStepButton()
 
-      const secondStepDescription = 'This is the second step'
-      const secondStepActor = 'Probation practitioner'
-      addStep.addStepAutocompleteText(2, secondStepDescription)
-      addStep.selectStepActor(2, secondStepActor)
+      const secondStep = DataGenerator.generateStep()
+      addStep.addStepAutocompleteText(2, secondStep.description)
+      addStep.selectStepActor(2, secondStep.actor)
 
       addStep.saveAndContinue()
 
@@ -134,8 +130,8 @@ describe('Add Steps', () => {
       addStep.removeStep(2)
       addStep.saveAndContinue()
       cy.get('table.goal-summary-card__steps .govuk-table__body').children().should('have.length', 1)
-      selectStepDescriptionByIndex(1).should('contain', firstStepDescription)
-      selectStepActorByIndex(1).should('contain', firstStepActor)
+      selectStepDescriptionByIndex(1).should('contain', firstStep.description)
+      selectStepActorByIndex(1).should('contain', firstStep.actor)
     })
   })
 
@@ -159,10 +155,10 @@ describe('Add Steps', () => {
 
     it('Save with incomplete step throws error', () => {
       cy.url().should('include', '/add-steps')
-      const firstStepDescription = 'This is the first step'
-      const firstStepActor = 'Sam'
-      addStep.addStepAutocompleteText(1, firstStepDescription)
-      addStep.selectStepActor(1, firstStepActor)
+
+      const firstStep = DataGenerator.generateStep()
+      addStep.addStepAutocompleteText(1, firstStep.description)
+      addStep.selectStepActor(1, firstStep.actor)
 
       addStep.addAnotherStepButton()
 
