acd7ccc50fae91bbc9e2aeb75bf2e9cf8701e758
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-10-04 17:56:17 +0100

    Make SP use assessment UUID from handover context

--- server/services/sessionService.ts
+++ server/services/sessionService.ts
@@ -12,16 +12,14 @@ export default class SessionService {
   setupSession = async () => {
     try {
       this.request.session.handover = await this.handoverContextService.getContext(this.request.user?.token)
-      this.request.session.plan = await this.planService.getPlanByOasysAssessmentPk(this.getOasysAssessmentPk())
+      this.request.session.plan = await this.planService.getPlanByUuid(this.getPlanUUID())
     } catch (e) {
       Logger.error('Failed to setup session:', e)
       throw Error(e)
     }
   }
 
-  getOasysAssessmentPk = () => this.request.session.handover?.sentencePlanContext?.oasysAssessmentPk
-
-  getPlanUUID = () => this.request.session.plan?.uuid
+  getPlanUUID = () => this.request.session.handover?.sentencePlanContext.planId
 
   getPrincipalDetails = () => this.request.session.handover?.principal
 
