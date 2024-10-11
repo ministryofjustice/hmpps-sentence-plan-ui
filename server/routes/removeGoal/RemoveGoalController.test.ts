f16c31afa4545a8dbc850a6ad102c967c7252bed
Phil Wilson <github@philwilson.org>
2024-10-11 18:22:53 +0100

    Adds new controller tests for removing a goal

--- server/routes/removeGoal/RemoveGoalController.test.ts
+++ server/routes/removeGoal/RemoveGoalController.test.ts
@@ -9,6 +9,7 @@ import URLs from '../URLs'
 jest.mock('../../services/sentence-plan/goalService', () => {
   return jest.fn().mockImplementation(() => ({
     deleteGoal: jest.fn().mockReturnValue({ status: 204 }),
+    removeGoal: jest.fn().mockReturnValue(testGoal),
     getGoal: jest.fn().mockReturnValue(testGoal),
     updateGoal: jest.fn().mockReturnValue(testGoal),
   }))
@@ -50,6 +51,47 @@ describe('Test Deleting Goal', () => {
       req.body = { type: 'some-type', action: 'delete', goalUuid: 'xyz' }
       await controller.post(req as Request, res as Response, next)
       expect(req.services.goalService.deleteGoal).toHaveBeenCalled()
+      expect(res.redirect).toHaveBeenCalledWith(`${URLs.PLAN_SUMMARY}?type=some-type&status=removed`)
+    })
+  })
+})
+
+describe('Test Removing Goal', () => {
+  let controller: RemoveGoalController
+  let req: Request
+  let res: Response
+  let next: NextFunction
+  const viewData = {
+    data: {
+      type: 'some-type',
+      goal: testGoal,
+      actionType: 'remove',
+    },
+    errors: {},
+    locale: locale.en.remove,
+  }
+
+  beforeEach(() => {
+    req = mockReq()
+    res = mockRes()
+    next = jest.fn()
+    req.query.type = 'some-type'
+    req.url = '/remove-goal'
+    controller = new RemoveGoalController()
+  })
+
+  describe('get', () => {
+    it('should render without validation errors', async () => {
+      await controller.get(req, res, next)
+      expect(res.render).toHaveBeenCalledWith('pages/remove-goal', viewData)
+    })
+  })
+
+  describe('post', () => {
+    it('should return to plan-summary after removing goal if remove goal is selected', async () => {
+      req.body = { type: 'some-type', action: 'remove', goalUuid: 'xyz' }
+      await controller.post(req as Request, res as Response, next)
+      expect(req.services.goalService.updateGoal).toHaveBeenCalled()
       expect(res.redirect).toHaveBeenCalledWith(`${URLs.PLAN_SUMMARY}?type=some-type&status=removed`)
     })
   })
