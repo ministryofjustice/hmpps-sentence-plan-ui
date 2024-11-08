2786bb4489a25f30139bba06d2e16ba6e949f3fe
Phil Wilson <github@philwilson.org>
2024-11-08 11:41:50 +0000

    Rebases onto main and updates function name correctly

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -12,8 +12,8 @@ import setupAgreePlanRoutes from './agree-plan/routes'
 import URLs from './URLs'
 import setupAddStepsRoutes from './add-steps/routes'
 import setupAchieveGoalRoutes from './achieveGoal/routes'
-import setupViewAchievedGoalRoutes from './viewGoalDetails/routes'
 import setupUpdateGoalRoutes from './update-goal/routes'
+import setupViewGoalDetailsRoutes from './viewGoalDetails/routes'
 
 export default function routes(services: Services): Router {
   const router = Router()
@@ -31,7 +31,7 @@ export default function routes(services: Services): Router {
   setupCreateGoalRoutes(router, services)
   setupRemoveGoalRoutes(router)
   setupChangeGoalRoutes(router, services)
-  setupViewAchievedGoalRoutes(router)
+  setupViewGoalDetailsRoutes(router)
   setupAchieveGoalRoutes(router)
   setupAddStepsRoutes(router)
   setupUpdateGoalRoutes(router, services)
--- server/routes/viewGoalDetails/routes.ts
+++ server/routes/viewGoalDetails/routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import URLs from '../URLs'
 import ViewGoalDetailsController from './ViewGoalDetailsController'
 
-export default function setupViewAchievedGoalRoutes(router: Router) {
+export default function setupViewGoalDetailsRoutes(router: Router) {
   const controller = new ViewGoalDetailsController()
 
   router.get(URLs.VIEW_ACHIEVED_GOAL, controller.get)
