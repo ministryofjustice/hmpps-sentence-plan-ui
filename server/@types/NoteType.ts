a245450989956bca37cf20b6b9b5c3160f8b28f1
izzyL-DJ <isaiah.lakanu@digital.justice.gov.uk>
2024-11-11 22:58:58 +0000

    plan history header now looks matches design doc, updated notetype to match data from BE

--- server/@types/NoteType.ts
+++ server/@types/NoteType.ts
@@ -2,7 +2,7 @@ export type NoteType = {
   noteObject: string // Plan or Goal
   note: string // text of note
   additionalNote: string
-  noteStatus: string // value of enum event matching the noteObject
+  noteType: string // value of enum event matching the noteObject
   goalTitle: string
   goalUuid: string
   createdDate: string
