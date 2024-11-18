d8c6fcdea80695fc536463cf5ad2c3ab018c14e0
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-11-18 11:33:12 +0000

    SP2-407: Removed print

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,6 @@
 import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
 import './datepicker.js'
-import './print.js'
 import './autocomplete/goal-autocomplete'
 import './autocomplete/step-autocomplete'
 
