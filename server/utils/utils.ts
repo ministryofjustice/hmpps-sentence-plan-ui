03cf2e7f3bd3503423ff8eeabcc4abeeb920a55b
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2024-11-19 16:08:22 +0000

    Update server/utils/utils.ts
    Co-authored-by: Thomas-Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>

--- server/utils/utils.ts
+++ server/utils/utils.ts
@@ -69,7 +69,7 @@ export const formatAssessmentData = (
   assessment: AssessmentResponse,
   areas: AssessmentAreaConfig[],
 ): AssessmentAreas => {
-  if (assessment === null || assessment === undefined || assessment.assessment === undefined) {
+  if (!assessment.assessment) {
     return { lowScoring: [], highScoring: [], other: [] }
   }
   const all = Object.values(areas)
