1239ecbaa2ab1bd5cb30efd7540371c24e593679
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-12-05 22:47:10 +0000

    Remove prom-metrics due to stability issues + not used

--- server.ts
+++ server.ts
@@ -7,7 +7,3 @@ import logger from './logger'
 app.listen(app.get('port'), () => {
   logger.info(`Server listening on port ${app.get('port')}`)
 })
-
-metricsApp.listen(metricsApp.get('port'), () => {
-  logger.info(`Metrics server listening on port ${metricsApp.get('port')}`)
-})
