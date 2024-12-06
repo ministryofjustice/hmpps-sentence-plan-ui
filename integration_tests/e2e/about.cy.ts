8f14ab43fa3068a55fe1f26a7ee64ed2cb0b2700
izzyL-DJ <isaiah.lakanu@digital.justice.gov.uk>
2024-12-06 13:12:49 +0000

    Introduced new table classes, updated cypress test

--- integration_tests/e2e/about.cy.ts
+++ integration_tests/e2e/about.cy.ts
@@ -1,23 +1,19 @@
-import DataGenerator from '../support/DataGenerator'
-
 describe('Rendering', () => {
   beforeEach(() => {
     cy.createSentencePlan().then(planDetails => {
       cy.wrap(planDetails).as('plan')
       cy.openSentencePlan(planDetails.oasysAssessmentPk)
-      cy.addGoalToPlan(planDetails.plan.uuid, DataGenerator.generateGoal()).then(goal => {
-        cy.addStepToGoal(goal.uuid, DataGenerator.generateStep())
-      })
-      cy.get('.moj-primary-navigation__container').contains('a', 'About').click()
+      // UNCOMMENTED WHEN ABOUT LINKS ARE RE-ADDED cy.get('.moj-primary-navigation__container').contains('a', 'About').click()
     })
   })
 
   it('Should check the page rendered correctly', () => {
+    cy.visit('/about')
     cy.url().should('include', '/about')
     cy.get('h1').should('include.text', 'About')
     cy.get('h2').contains('Sentence information')
     cy.get('h2').contains('High-scoring areas from the assessment')
-    cy.get('.about-table-header')
+    cy.get('.govuk-body-s')
       .invoke('text')
       .then(text => {
         expect(text).to.include('Sentence')
@@ -25,5 +21,10 @@ describe('Rendering', () => {
         expect(text).to.include('Unpaid work')
         expect(text).to.include('RAR (Rehabilitation activity requirement)')
       })
+  })
+
+  it('Should check if the entries are displayed correctly', () => {
+    cy.visit('/about')
+    cy.get('.about-table-body').invoke('text').should('not.be.empty') // check table contents are not empty until we can check for specifics
   })
 })
