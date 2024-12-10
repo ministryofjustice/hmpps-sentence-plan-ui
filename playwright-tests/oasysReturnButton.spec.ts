ae4626eb417b5abf9e414f974bca5d5c3d881af5
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-12-10 17:36:45 +0000

    Fix playwright test

--- playwright-tests/oasysReturnButton.spec.ts
+++ playwright-tests/oasysReturnButton.spec.ts
@@ -1,18 +1,17 @@
 import { test, expect } from '@playwright/test'
 import { createSentencePlan, openSentencePlan } from './sentencePlanSession.setup'
 
-let sentencePlanPage: any
+test.describe('OASys Return Button', () => {
+  let sentencePlanPage
 
-test.beforeAll(async () => {
-  const sentencePlan = await createSentencePlan()
-  sentencePlanPage = await openSentencePlan(sentencePlan.oasysAssessmentPk)
-})
+  test.beforeAll(async () => {
+    const sentencePlan = await createSentencePlan()
+    sentencePlanPage = await openSentencePlan(sentencePlan.oasysAssessmentPk)
+  })
 
-test('Displays and references the button correctly on the plan overview page', async () => {
-  const returnButton = await sentencePlanPage.getByRole('button', { name: 'Return to OASys' })
-  await expect(returnButton).toBeVisible()
-  const buttonHref = await returnButton.getAttribute('href')
-  expect(buttonHref).not.toBeNull()
-  expect(buttonHref).not.toBe('')
-  expect(buttonHref).toBe(process.env.OASTUB_URL)
+  test('should display and reference the return button correctly on the plan overview page', async () => {
+    const returnButton = sentencePlanPage.getByRole('button', { name: 'Return to OASys' })
+    await expect(returnButton).toBeVisible()
+    await expect(returnButton).toHaveAttribute('href', process.env.OASTUB_URL)
+  })
 })
