49ecd0e7ff3822553331765505927e3151811e8c
Phil Wilson <github@philwilson.org>
2024-12-11 10:34:42 +0000

    Break handover into beforeall

--- playwright-tests/test-1.spec.ts
+++ playwright-tests/test-1.spec.ts
@@ -1,23 +1,29 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, Page } from '@playwright/test'
 import { createSentencePlan, openSentencePlan } from './sentencePlanSession.setup'
 
-test('test', async () => {
-  const sentencePlan = await createSentencePlan()
-  const sentencePlanPage = await openSentencePlan(sentencePlan.oasysAssessmentPk)
+test.describe('Simple create goal test', () => {
+  let sentencePlanPage: Page
 
-  await sentencePlanPage.goto('/plan')
-  await expect(sentencePlanPage.locator('h1')).toContainText('plan')
+  test.beforeAll(async () => {
+    const sentencePlan = await createSentencePlan()
+    sentencePlanPage = await openSentencePlan(sentencePlan.oasysAssessmentPk)
+  })
 
-  await sentencePlanPage.getByRole('button', { name: 'Create goal' }).click()
-  await sentencePlanPage.getByLabel(/What goal should/).click()
-  await sentencePlanPage.getByLabel(/What goal should/).fill('This is the goal title')
-  await sentencePlanPage.getByLabel('No', { exact: true }).check()
-  await sentencePlanPage
-    .getByRole('group', { name: /start working on this goal now/ })
-    .getByLabel('Yes')
-    .check()
-  await sentencePlanPage.getByLabel(/In 3 months/).check()
-  await sentencePlanPage.getByRole('button', { name: 'Save without steps' }).click()
-  await expect(sentencePlanPage.locator('h2')).toContainText('This is the goal title')
-  await sentencePlanPage.screenshot({ path: 'test-results/create-goal.png' })
+  test('test', async () => {
+    await sentencePlanPage.goto('/plan')
+    await expect(sentencePlanPage.locator('h1')).toContainText('plan')
+
+    await sentencePlanPage.getByRole('button', { name: 'Create goal' }).click()
+    await sentencePlanPage.getByLabel(/What goal should/).click()
+    await sentencePlanPage.getByLabel(/What goal should/).fill('This is the goal title')
+    await sentencePlanPage.getByLabel('No', { exact: true }).check()
+    await sentencePlanPage
+      .getByRole('group', { name: /start working on this goal now/ })
+      .getByLabel('Yes')
+      .check()
+    await sentencePlanPage.getByLabel(/In 3 months/).check()
+    await sentencePlanPage.getByRole('button', { name: 'Save without steps' }).click()
+    await expect(sentencePlanPage.locator('h2')).toContainText('This is the goal title')
+    await sentencePlanPage.screenshot({ path: 'test-results/create-goal.png' })
+  })
 })
