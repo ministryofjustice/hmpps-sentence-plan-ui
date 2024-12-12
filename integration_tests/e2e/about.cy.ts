78db7379ac31b1d2bb0f2008dc566aca1a5c1995
izzyL-DJ <isaiah.lakanu@digital.justice.gov.uk>
2024-12-12 11:20:05 +0000

    Updated some tests to be more readable, added test to check lnks in each assessment section is correct

--- integration_tests/e2e/about.cy.ts
+++ integration_tests/e2e/about.cy.ts
@@ -4,9 +4,9 @@ describe('Rendering', () => {
       cy.wrap(planDetails).as('plan')
       cy.openSentencePlan(planDetails.oasysAssessmentPk)
       cy.get('.moj-primary-navigation__container').contains('a', 'About').click()
-      // cy.get(
-      //   ':nth-child(4) > .govuk-accordion__controls > .govuk-accordion__show-all > .govuk-accordion__show-all-text',
-      // ).click()
+      cy.get(
+        ':nth-child(4) > .govuk-accordion__controls > .govuk-accordion__show-all > .govuk-accordion__show-all-text',
+      ).click()
     })
   })
 
@@ -46,68 +46,67 @@ describe('Rendering', () => {
     cy.get('.govuk-accordion__section-heading-text-focus')
       .then($red => $red.toArray().map(el => el.innerText.trim())) // trim whitespace
       .should('deep.equal', expectedText)
-    cy.get(
-      ':nth-child(4) > :nth-child(3) > .govuk-accordion__section-header > .govuk-accordion__section-heading > .govuk-accordion__section-button',
-    )
-      .find('#accordion-with-summary-sections-summary-2 > .govuk-accordion__section-summary-focus > .moj-badge')
-      .contains('Risk of reoffending')
-    cy.get(
-      ':nth-child(5) > .govuk-accordion__section-header > .govuk-accordion__section-heading > .govuk-accordion__section-button',
-    )
-      .find('#accordion-with-summary-sections-summary-4 > .govuk-accordion__section-summary-focus > .moj-badge')
-      .contains('Risk of reoffending')
-    cy.get(
-      ':nth-child(6) > .govuk-accordion__section-header > .govuk-accordion__section-heading > .govuk-accordion__section-button',
-    )
-      .find('#accordion-with-summary-sections-summary-5 > .govuk-accordion__section-summary-focus > .moj-badge')
-      .contains('Risk of reoffending')
+  })
+  it('Should check the hard-coded labels appear next to the correct, predetermined areas in correct order', () => {
+    cy.get('.govuk-accordion__section-summary-focus').eq(1).contains('Risk of reoffending')
+    cy.get('.govuk-accordion__section-summary-focus').eq(3).contains('Risk of reoffending')
+    cy.get('.govuk-accordion__section-summary-focus').eq(4).contains('Risk of reoffending')
   })
 
-  it('Should check if the hard-coded data for high-scoring areas are displayed correctly and in order', () => {
-    cy.get(
-      ':nth-child(4) > .govuk-accordion__controls > .govuk-accordion__show-all > .govuk-accordion__show-all-text',
-    ).click()
-    cy.get(':nth-child(4) > :nth-child(3) > #accordion-default-content-2 > :nth-child(1)').contains(
-      'This area is not linked to RoSH (risk of serious harm)',
-    )
-    cy.get('#accordion-default-content-1 > .motivation > .govuk-heading-s ').contains(
-      'Motivation to make changes in this area',
-    )
-    cy.get('#accordion-default-content-1 > .motivation > .govuk-body').contains(
-      'Sam wants to make changes but needs help.',
-    )
-    cy.get(':nth-child(4) > :nth-child(2) > #accordion-default-content-1 > :nth-child(4)').contains(
-      'There are no strengths or protective factors related to this area',
-    )
-    cy.get('#accordion-default-content-1 > :nth-child(5) > .govuk-heading-s').contains(
-      'Thinking, behaviours and attitudes need score',
-    )
-    cy.get('#accordion-default-content-1 > :nth-child(5) > p.govuk-body').contains(
-      '10 out of 10. (Scores above 2 are high-scoring.)',
-    )
-    cy.get(
-      '#accordion-default-content-1 > :nth-child(5) > .govuk-grid-row > .govuk-grid-column-full > .needs-score > .needs-score-label-wrapper > .needs-score-label__card > h3',
-    ).contains('10')
-    cy.get(
-      '#accordion-default-content-1 > :nth-child(5) > .govuk-grid-row > .govuk-grid-column-full > .needs-score > .needs-score-label-wrapper > .needs-score-label__card > p',
-    ).contains('out of 10')
-    cy.get(':nth-child(7) > .govuk-heading-s').contains('Lifestyle and associates need score')
-    cy.get(':nth-child(7) > p.govuk-body').contains('6 out of 6. (Scores above 1 are high-scoring.)')
-    cy.get(
-      ':nth-child(7) > .govuk-grid-row > .govuk-grid-column-full > .needs-score > .needs-score-label-wrapper > .needs-score-label__card > h3',
-    ).contains('6')
-    cy.get(
-      ':nth-child(7) > .govuk-grid-row > .govuk-grid-column-full > .needs-score > .needs-score-label-wrapper > .needs-score-label__card > p',
-    ).contains('out of 6')
-    cy.get(':nth-child(4) > :nth-child(2) > #accordion-default-content-1')
-      .find(':nth-child(9) > .goal-link > .govuk-link')
+  it('Should check all hard-coded links in each assessment section are in the expected order', () => {
+    cy.get('p.goal-link')
+      .eq(0)
       .contains('Create thinking, behaviours and attitudes goal')
       .should('have.attr', 'href')
       .and('include', '/create-goal/thinking-behaviours-and-attitudes')
-    cy.get(':nth-child(4) > :nth-child(3) > #accordion-default-content-2')
-      .find(':nth-child(4) > :nth-child(3) > #accordion-default-content-2 > :nth-child(6) > .goal-link > .govuk-link')
+    cy.get('p.goal-link')
+      .eq(1)
       .contains('Create accommodation goal')
       .should('have.attr', 'href')
-      .and('include', '/create-goal/Accomodation')
+      .and('include', '/create-goal/accommodation')
+    cy.get('p.goal-link')
+      .eq(2)
+      .contains('Create personal relationships and community goal')
+      .should('have.attr', 'href')
+      .and('include', '/create-goal/personal-relationships-and-community')
+    cy.get('p.goal-link')
+      .eq(3)
+      .contains('Create alcohol use goa')
+      .should('have.attr', 'href')
+      .and('include', '/create-goal/alcohol-use')
+    cy.get('p.goal-link')
+      .eq(4)
+      .contains('Create employment and education goal')
+      .should('have.attr', 'href')
+      .and('include', '/create-goal/employment-and-education')
+    cy.get('p.goal-link')
+      .eq(5)
+      .contains('Create drug use goal')
+      .should('have.attr', 'href')
+      .and('include', '/create-goal/drug-use')
+    cy.get('p.goal-link')
+      .eq(6)
+      .contains('Create finances goal')
+      .should('have.attr', 'href')
+      .and('include', '/create-goal/finances')
+    cy.get('p.goal-link')
+      .eq(7)
+      .contains('Create health and wellbeing goal')
+      .should('have.attr', 'href')
+      .and('include', '/create-goal/health-and-wellbeing')
+  })
+
+  it('Should check if the hard-coded data for high-scoring areas are displayed correctly and in order', () => {
+    cy.get('h3.govuk-heading-s').eq(0).contains('This area is not linked to RoSH (risk of serious harm)')
+    cy.get('h3.govuk-heading-s').eq(1).contains('This area is not linked to risk of reoffending')
+    cy.get('h3.govuk-heading-s').eq(2).contains('Motivation to make changes in this area')
+    cy.get('div.motivation').eq(0).contains('Sam wants to make changes but needs help')
+    cy.get('h3.govuk-heading-s').eq(3).contains('There are no strengths or protective factors related to this area')
+    cy.get('h3.govuk-heading-s').eq(4).contains('Thinking, behaviours and attitudes need score')
+    cy.get('p.govuk-body').eq(2).contains('10 out of 10. (Scores above 2 are high-scoring')
+    cy.get('.needs-score-label__card').eq(0).contains('10 out of 10')
+    cy.get('h3.govuk-heading-s').eq(5).contains('Lifestyle and associates need score')
+    cy.get('p.govuk-body').eq(3).contains('6 out of 6. (Scores above 1 are high-scoring.)')
+    cy.get('.needs-score-label__card').eq(1).contains('6 out of 6')
   })
 })
