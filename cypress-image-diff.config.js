f30cc23db36bff89f86662ea838f8d16de3e0cc0
Phil Wilson <github@philwilson.org>
2024-12-20 17:08:02 +0000

    Have a go at generating the HTML report for the visual diff

--- cypress-image-diff.config.js
+++ cypress-image-diff.config.js
@@ -1,5 +1,9 @@
 const config = {
   FAILURE_THRESHOLD: 0.05, // 5% difference allowed
+  JSON_REPORT: {
+    FILENAME: 'cypress-image-diff',
+    OVERWRITE: true,
+  },
 }
 
 module.exports = config
