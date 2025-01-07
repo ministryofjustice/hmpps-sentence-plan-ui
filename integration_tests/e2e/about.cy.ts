39fb8aa7c1e63774bf554028ff67dfde75ce5063
izzyL-DJ <109157122+izzyL-DJ@users.noreply.github.com>
2025-01-07 16:01:37 +0000

    SP2-1101 About Page Cypress test extra tests (#326)
    Co-authored-by: Thomas-Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>

--- integration_tests/e2e/about.cy.ts
+++ integration_tests/e2e/about.cy.ts
@@ -37,9 +37,9 @@ describe('Rendering READ_WRITE', () => {
 
   it('Should check if main titles of hard-coded high-scoring areas from the assessment are displayed correctly and in order with the correct risk marker', () => {
     const expectedText = [
-      'Thinking, behaviours and attitudes',
       'Accommodation',
       'Personal relationships and community',
+      'Thinking, behaviours and attitudes',
       'Alcohol use',
       'Employment and education',
       'Drug use',
@@ -61,9 +61,9 @@ describe('Rendering READ_WRITE', () => {
     cy.get('.govuk-accordion__show-all').click({ multiple: true })
 
     const areas = [
-      { text: 'Create thinking, behaviours and attitudes goal', href: 'thinking-behaviours-and-attitudes' },
       { text: 'Create accommodation goal', href: 'accommodation' },
       { text: 'Create personal relationships and community goal', href: 'personal-relationships-and-community' },
+      { text: 'Create thinking, behaviours and attitudes goal', href: 'thinking-behaviours-and-attitudes' },
       { text: 'Create alcohol use goal', href: 'alcohol-use' },
       { text: 'Create employment and education goal', href: 'employment-and-education' },
       { text: 'Create drug use goal', href: 'drug-use' },
@@ -87,15 +87,15 @@ describe('Rendering READ_WRITE', () => {
       'Motivation to make changes in this area Sam wants to make changes but needs help. ' +
       'There are no strengths or protective factors related to this area ' +
       'Thinking, behaviours and attitudes need score ' +
-      '10 out of 10. (Scores above 2 are high-scoring.) ' +
-      '10 out of 10 ' +
+      '1 out of 10. (Scores above 2 are high-scoring.) ' +
+      '1 out of 10 ' +
       'Lifestyle and associates need score ' +
       '6 out of 6. (Scores above 1 are high-scoring.) ' +
       '6 out of 6 ' +
       'Create thinking, behaviours and attitudes goal'
-    cy.get('.govuk-accordion__show-all').first().click() // click show all in high-scoring assessment section
+    cy.get('.govuk-accordion__show-all').eq(0).click() // click show all in high-scoring assessment section
     cy.contains('.govuk-accordion__section', 'Thinking, behaviours and attitudes')
-      .find('#accordion-default-content-1')
+      .find('#accordion-default-content-3')
       .invoke('text')
       .then(text => {
         const trimText = text.trim().replace(/\s+/g, ' ') // regex to catch and replace excessive newlines to single whitespace
@@ -109,6 +109,16 @@ describe('Rendering READ_WRITE', () => {
       .should('have.length', 6)
   })
 
+  it('Should check if the score graph for (high-scoring area) thinking behaviour and attitudes is displayed correctly', () => {
+    cy.get('.govuk-accordion__show-all').eq(0).click()
+    cy.contains('.assessment-score', 'Thinking, behaviours and attitudes need score')
+      .find('.lowscoring')
+      .should('have.length', 1)
+    cy.contains('.assessment-score', 'Lifestyle and associates need score')
+      .find('.highscoring')
+      .should('have.length', 6)
+  })
+
   it('Should check if the data for (low-scoring area) drug use are displayed correctly and in order', () => {
     const expectedHeadings =
       'This area is not linked to RoSH (risk of serious harm) ' +
@@ -118,7 +128,7 @@ describe('Rendering READ_WRITE', () => {
       '0 out of 8. (Scores above 0 are high-scoring.) ' +
       '0 out of 8 ' +
       'Create drug use goal'
-    cy.get('.govuk-accordion__show-all').eq(1).click()
+    cy.get('.govuk-accordion__show-all').eq(1).click() // click show all in low-scoring assessment section
     cy.contains('.govuk-accordion__section', 'Drug use')
       .find('#accordion-default-content-1')
       .invoke('text')
@@ -130,6 +140,11 @@ describe('Rendering READ_WRITE', () => {
     cy.contains('.govuk-accordion__section', 'Drug use').find('.govuk-body').should('have.length', 3)
   })
 
+  it('Should check if the score graph for (low-scoring area) drug use is displayed correctly', () => {
+    cy.get('.govuk-accordion__show-all').eq(1).click()
+    cy.contains('.assessment-score', '0 out of 8').find('.highscoring').should('have.length', 0)
+  })
+
   it('Should check if the data for (non-scoring area) health and wellbeing are displayed correctly and in order', () => {
     const expectedHeadings =
       'This area is not linked to RoSH (risk of serious harm) ' +
@@ -139,7 +154,7 @@ describe('Rendering READ_WRITE', () => {
       'There are no strengths or protective factors related to this area ' +
       'This area does not have a need score ' +
       'Create health and wellbeing goal'
-    cy.get('.govuk-accordion__show-all').eq(2).click()
+    cy.get('.govuk-accordion__show-all').eq(2).click() // click show all in non-scoring assessment section
     cy.contains('.govuk-accordion__section', 'Health and wellbeing')
       .find('#accordion-default-content-2')
       .invoke('text')
