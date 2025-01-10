6264b285af3d90ff378eb9ad08016c3fab14b445
Phil Wilson <github@philwilson.org>
2025-01-10 12:20:38 +0000

    Break the app

--- server/routes/reAddGoal/models/ReAddGoalPostModel.ts
+++ server/routes/reAddGoal/models/ReAddGoalPostModel.ts
@@ -1,6 +1,4 @@
-import { IsNotEmpty, MaxLength, Validate, ValidateIf } from 'class-validator'
-import { Expose } from 'class-transformer'
-import GoalDateMustBeTodayOrFuture from '../../validators/GoalDateMustBeTodayOrFuture'
+import { IsNotEmpty, MaxLength } from 'class-validator'
 
 export default class ReAddGoalPostModel {
   @IsNotEmpty()
@@ -10,13 +8,13 @@ export default class ReAddGoalPostModel {
   @IsNotEmpty()
   'start-working-goal-radio': string
 
-  @ValidateIf(o => o['start-working-goal-radio'] === 'yes')
-  @IsNotEmpty()
-  'date-selection-radio': string
-
-  @ValidateIf(o => o['date-selection-radio'] === 'custom' && o['start-working-goal-radio'] === 'yes')
-  @IsNotEmpty()
-  @Validate(GoalDateMustBeTodayOrFuture, { message: 'Date must be today or in the future' })
-  @Expose()
-  'date-selection-custom': string
+  // @ValidateIf(o => o['start-working-goal-radio'] === 'yes')
+  // @IsNotEmpty()
+  // 'date-selection-radio': string
+  //
+  // @ValidateIf(o => o['date-selection-radio'] === 'custom' && o['start-working-goal-radio'] === 'yes')
+  // @IsNotEmpty()
+  // @Validate(GoalDateMustBeTodayOrFuture, { message: 'Date must be today or in the future' })
+  // @Expose()
+  // 'date-selection-custom': string
 }
