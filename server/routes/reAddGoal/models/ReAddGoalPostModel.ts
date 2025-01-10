f7bf4bae70207184f61b3870d5fad5fc5fba8ae2
Phil Wilson <github@philwilson.org>
2025-01-10 12:13:29 +0000

    WIP

--- server/routes/reAddGoal/models/ReAddGoalPostModel.ts
+++ server/routes/reAddGoal/models/ReAddGoalPostModel.ts
@@ -1,6 +1,22 @@
-import { MaxLength } from 'class-validator'
+import { IsNotEmpty, MaxLength, Validate, ValidateIf } from 'class-validator'
+import { Expose } from 'class-transformer'
+import GoalDateMustBeTodayOrFuture from '../../validators/GoalDateMustBeTodayOrFuture'
 
 export default class ReAddGoalPostModel {
+  @IsNotEmpty()
   @MaxLength(4000)
-  'goal-achievement-helped': string
+  're-add-goal-reason': string
+
+  @IsNotEmpty()
+  'start-working-goal-radio': string
+
+  @ValidateIf(o => o['start-working-goal-radio'] === 'yes')
+  @IsNotEmpty()
+  'date-selection-radio': string
+
+  @ValidateIf(o => o['date-selection-radio'] === 'custom' && o['start-working-goal-radio'] === 'yes')
+  @IsNotEmpty()
+  @Validate(GoalDateMustBeTodayOrFuture, { message: 'Date must be today or in the future' })
+  @Expose()
+  'date-selection-custom': string
 }
