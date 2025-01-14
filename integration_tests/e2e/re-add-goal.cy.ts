2931d39ba752774047fc8d5b71096adb9847454a
Phil Wilson <github@philwilson.org>
2025-01-14 11:36:19 +0000

    WIP

--- integration_tests/e2e/re-add-goal.cy.ts
+++ integration_tests/e2e/re-add-goal.cy.ts
@@ -14,6 +14,12 @@ describe('Re-add a goal to a Plan after it has been removed', () => {
         cy.removeGoalFromPlan(goal.uuid, 'A removal note')
         removedGoal = goal
       })
+
+      // add a second goal so that we can test that the order changes when adding back
+      // cy.addGoalToPlan(planDetails.plan.uuid, DataGenerator.generateGoal()).then(goal => {
+      //   cy.addStepToGoal(goal.uuid, DataGenerator.generateStep())
+      //   cy.agreePlan(planDetails.plan.uuid)
+      // })
     })
   })
 
@@ -58,6 +64,25 @@ describe('Re-add a goal to a Plan after it has been removed', () => {
     cy.get('button').contains('Confirm').click()
 
     cy.visit('/plan-history')
+    cy.get('.goal-status').first().should('contain.text', 'Goal added back into plan')
     cy.get('.goal-note').first().should('contain.text', RE_ADD_REASON)
+  })
+
+  it('Re-adding a goal puts it at the bottom of the goal list on the Plan Overview', () => {
+    const RE_ADD_REASON = 'A reason for re-adding the goal'
+
+    // add a second goal
+    // confirm that the one to remove is first
+    // TODO
+
+    cy.visit(`/view-removed-goal/${removedGoal.uuid}`)
+    cy.get('a.add-to-plan').click()
+    cy.get('#re-add-goal-reason').type(RE_ADD_REASON)
+    cy.get('input[name="start-working-goal-radio"][value="no"]').click()
+    cy.get('button').contains('Confirm').click()
+
+    // todo
+    // visit plan overview
+    // check order
   })
 })
