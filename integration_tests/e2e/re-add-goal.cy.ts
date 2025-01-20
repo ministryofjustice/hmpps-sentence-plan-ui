37d24a2ac03f5ff486d20671ee3d929e45c43dbb
Phil Wilson <pipwilson@users.noreply.github.com>
2025-01-20 13:58:18 +0000

    Updates following design review (#353)

--- integration_tests/e2e/re-add-goal.cy.ts
+++ integration_tests/e2e/re-add-goal.cy.ts
@@ -29,6 +29,13 @@ describe('Re-add a goal to a Plan after it has been removed', () => {
     cy.title().should('contain', 'Confirm you want to add this goal back into the plan')
   })
 
+  it('Back button on re-add goal confirmation links to goal details page', () => {
+    cy.visit(`/view-removed-goal/${removedGoal.uuid}`)
+    cy.get('a.add-to-plan').click()
+    cy.get('a').contains('Do not add goal back into plan').click()
+    cy.title().should('contain', 'View removed goal')
+  })
+
   it('Confirming re-add goal with target date loads plan overview on current goals tab', () => {
     cy.visit(`/view-removed-goal/${removedGoal.uuid}`)
     cy.get('a.add-to-plan').click()
@@ -59,6 +66,20 @@ describe('Re-add a goal to a Plan after it has been removed', () => {
 
     cy.visit('/plan-history')
     cy.get('.goal-status').first().should('contain.text', 'Goal added back into plan')
+    cy.get('.goal-note').first().should('contain.text', RE_ADD_REASON)
+  })
+
+  it('Re-adding a goal adds a note to the Goal notes', () => {
+    const RE_ADD_REASON = 'A reason for re-adding the goal'
+
+    cy.visit(`/view-removed-goal/${removedGoal.uuid}`)
+    cy.get('a.add-to-plan').click()
+    cy.get('#re-add-goal-reason').type(RE_ADD_REASON)
+    cy.get('input[name="start-working-goal-radio"][value="no"]').click()
+    cy.get('button').contains('Confirm').click()
+
+    cy.visit(`/update-goal-steps/${removedGoal.uuid}`)
+    cy.get('.goal-status').first().should('contain.text', 'Goal added back into plan on')
     cy.get('.goal-note').first().should('contain.text', RE_ADD_REASON)
   })
 })
