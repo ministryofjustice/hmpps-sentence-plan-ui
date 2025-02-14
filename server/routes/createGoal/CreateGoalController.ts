6bb3a36542cdd10fd27e001bf9feab628e4bc260
Phil Wilson <pipwilson@users.noreply.github.com>
2025-02-14 12:57:33 +0000

    SP2-1225 Add assessment info to Change Goal and Add Steps (#393)

--- server/routes/createGoal/CreateGoalController.ts
+++ server/routes/createGoal/CreateGoalController.ts
@@ -55,19 +55,15 @@ export default class CreateGoalController {
       const planUuid = req.services.sessionService.getPlanUUID()
       const plan = await req.services.planService.getPlanByUuid(planUuid)
 
-      let assessmentDetailsForArea = null
-
-      // get assessment data
-      try {
-        const assessmentResponse = await req.services.assessmentService.getAssessmentByUuid(planUuid)
-        const assessmentData = assessmentResponse.sanAssessmentData
-
-        // 2. get the assessment details for the selected area of need
-        const areaConfig: AssessmentAreaConfig = areaConfigs.find(config => config.area === selectedAreaOfNeed?.name)
-        assessmentDetailsForArea = getAssessmentDetailsForArea(criminogenicNeedsData, areaConfig, assessmentData)
-      } catch {
-        /* swallow the error, missing data is handled in the template */
-      }
+      // get assessment data or swallow the service error and set to null so the template knows this data is missing
+      const assessmentDetailsForArea = await req.services.assessmentService
+        .getAssessmentByUuid(planUuid)
+        .then(assessmentResponse => {
+          const assessmentData = assessmentResponse.sanAssessmentData
+          const areaConfig: AssessmentAreaConfig = areaConfigs.find(config => config.area === selectedAreaOfNeed?.name)
+          return getAssessmentDetailsForArea(criminogenicNeedsData, areaConfig, assessmentData)
+        })
+        .catch((): null => null)
 
       req.services.sessionService.setReturnLink(null)
 
