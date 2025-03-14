8858895078479852115803dbdd67619ce61b61e5
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2025-03-14 16:28:39 +0000

    Add `custom-deployment-banner` component to highlight when viewing an ephemeral deployment
    (cherry picked from commit 094b9381f4bf6993b3b0b787b4c87a3aa7ab9adc)

--- server/config.ts
+++ server/config.ts
@@ -121,4 +121,5 @@ export default {
   },
   domain: get('INGRESS_URL', 'http://localhost:3000', requiredInProduction),
   environmentName: get('ENVIRONMENT_NAME', ''),
+  deploymentName: get('DEPLOYMENT_NAME', ''),
 }
