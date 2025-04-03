edd39dd0261f41ffa112fe02684500a47696a0be
Aiden Herbert <Aiden.Herbert@justice.gov.uk>
2025-04-03 15:46:34 +0100

    Removal of imports added into index.js to resolve UI bug and console error.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,6 @@
 import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
-import * as hmrcFrontend from 'hmrc-frontend'
 import './autocomplete/goal-autocomplete'
 
 govukFrontend.initAll()
 mojFrontend.initAll()
-hmrcFrontend.initAll()
