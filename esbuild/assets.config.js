f9dda38236b708687fcec90a4e4f688578026077
Aiden Herbert <Aiden.Herbert@justice.gov.uk>
2025-04-09 13:15:02 +0100

    Ensure paths for hmrc-frontend are set accordingly to prevent UI console errors.

--- esbuild/assets.config.js
+++ esbuild/assets.config.js
@@ -25,7 +25,7 @@ const buildAssets = buildConfig =>
     sourcemap: !buildConfig.isProduction,
     platform: 'browser',
     target: 'es2018',
-    external: ['/assets/*', 'hmrc-frontend', '/hmrc-frontend/*'],
+    external: ['/assets/*', 'node_modules/hmrc-frontend/*'],
     bundle: true,
     plugins: [
       clean({
