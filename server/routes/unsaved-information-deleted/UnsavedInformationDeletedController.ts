a686f579300fa689c5fb32dfff5ff3dde64c075b
Aiden Herbert <Aiden.Herbert@justice.gov.uk>
2025-04-30 16:38:56 +0100

    Update controller logic and page template to ensure additional details dont render as per page designs and requirements.

--- server/routes/unsaved-information-deleted/UnsavedInformationDeletedController.ts
+++ server/routes/unsaved-information-deleted/UnsavedInformationDeletedController.ts
@@ -1,21 +1,11 @@
 import { NextFunction, Request, Response } from 'express'
 import locale from './locale.json'
-import { HttpError } from '../../utils/HttpError'
 
 export default class UnsavedInformationDeletedController {
   private render = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const subjectData = await req.services.sessionService.getSubjectDetails()
-
-      return res.render('pages/unsaved-information-deleted', {
-        locale: locale.en,
-        data: {
-          subjectData,
-        },
-      })
-    } catch (e) {
-      return next(HttpError(500, e.message))
-    }
+    return res.render('pages/unsaved-information-deleted', {
+      locale: locale.en,
+    })
   }
 
   get = [this.render]
