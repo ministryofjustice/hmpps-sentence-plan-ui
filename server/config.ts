f5273c439fe2438b36f86277f7e80b40efcf4c98
Lucas Cairns <lucas.cairns@gmail.com>
2025-05-27 12:18:51 +0100

    ACE-99 - Add feedback banner to pages

--- server/config.ts
+++ server/config.ts
@@ -122,4 +122,5 @@ export default {
   domain: get('INGRESS_URL', 'http://localhost:3000', requiredInProduction),
   environmentName: get('ENVIRONMENT_NAME', ''),
   deploymentName: get('DEPLOYMENT_NAME', ''),
+  feedbackUrl: get('FEEDBACK_URL', null),
 }
