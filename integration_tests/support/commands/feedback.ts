26b7819960898c3b9cddb6e2c9ce7d6e42ef17aa
Lucas Cairns <lucas.cairns@gmail.com>
2025-05-27 18:23:25 +0100

    Fix lint

--- integration_tests/support/commands/feedback.ts
+++ integration_tests/support/commands/feedback.ts
@@ -1,14 +1,12 @@
+// eslint-disable-next-line import/prefer-default-export
 export const hasFeedbackLink = () => {
   const stubFeedbackUrl = 'http://localhost:9092/'
 
   cy.get('.govuk-phase-banner__text > .govuk-link').as('feedbackLink')
 
-  cy.get('@feedbackLink')
-    .should('have.attr', 'target').and('equal', '_blank')
+  cy.get('@feedbackLink').should('have.attr', 'target').and('equal', '_blank')
 
-  cy.get('@feedbackLink')
-    .invoke('attr', 'target', '_self')
-    .click()
+  cy.get('@feedbackLink').invoke('attr', 'target', '_self').click()
 
   cy.url().should('equal', stubFeedbackUrl)
 
