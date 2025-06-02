798b516f8a1032fced9f17e9fab4c5dd76086855
Aiden Herbert <Aiden.Herbert@justice.gov.uk>
2025-06-02 17:12:34 +0100

    Initial restructuring of VRT and first 4 UI states of create goal.

--- integration_tests/e2e/visual-comparison.cy.ts
+++ integration_tests/e2e/visual-comparison.cy.ts
@@ -1,28 +1,55 @@
-describe('Rendering of pages we can GET', () => {
-  before(() => {
+describe('Visual comparison testing', () => {
+  beforeEach(() => {
     cy.createSentencePlan().then(planDetails => {
       cy.wrap(planDetails).as('plan')
       cy.openSentencePlan(planDetails.oasysAssessmentPk)
+      cy.visit('/plan')
     })
   })
 
-  it('take plan page screenshot', () => {
-    cy.visit('/plan')
-    cy.compareSnapshot('plan-page')
+  describe('Plan overview', () => {
+    it('Plan overview page screenshot', () => {
+      cy.compareSnapshot('plan-overview-page')
+    })
   })
 
-  it('take create goal page screenshot', () => {
-    cy.visit('/create-goal/accommodation')
-    cy.compareSnapshot('create-goal-page')
+  describe('About', () => {
+    it('About page screenshot', () => {
+      cy.get('.moj-primary-navigation__container').contains('a', 'About').click()
+      cy.compareSnapshot('about-page')
+    })
   })
 
-  it('take plan history page screenshot', () => {
-    cy.visit('/plan-history')
-    cy.compareSnapshot('plan-history-page')
+  describe('Create goal', () => {
+    beforeEach(() => {
+      cy.contains('a', 'Create goal').click()
+    })
+
+    it('Create goal - accommodation screenshot', () => {
+      cy.get('.moj-side-navigation__list').contains('a', 'Accommodation').click()
+      cy.compareSnapshot('create-goal-accommodation-page')
+    })
+
+    it('Create goal - employment and education screenshot', () => {
+      cy.get('.moj-side-navigation__list').contains('a', 'Employment and education').click()
+      cy.compareSnapshot('create-goal-employment-and-education-page')
+    })
+
+    it('Create goal - finances screenshot', () => {
+      cy.get('.moj-side-navigation__list').contains('a', 'Finances').click()
+      cy.compareSnapshot('create-goal-finances-page')
+    })
+
+    it('Create goal - drug use screenshot', () => {
+      cy.get('.moj-side-navigation__list').contains('a', 'Drug use').click()
+      cy.compareSnapshot('create-goal-drug-use-page')
+    })
   })
 
-  it('take about page screenshot', () => {
-    cy.visit('/about')
-    cy.compareSnapshot('about-page')
-  })
+  // describe('Plan History', () => {
+  //   it('take plan history page screenshot', () => {
+  //     cy.visit('/plan-history')
+  //     cy.compareSnapshot('plan-history-page')
+  //   })
+  // })
 })
