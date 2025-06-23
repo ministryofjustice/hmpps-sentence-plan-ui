5550ac5d040beaf20e098fb3c5b4a26167a080f9
Aiden Herbert <Aiden.Herbert@justice.gov.uk>
2025-06-23 13:24:59 +0100

    Account of UI states for the addition of a data privicy screen to the UI flow.

---
+++ integration_tests/e2e/vrt-tests/data-privacy.cy.ts
@@ -0,0 +1,23 @@
+describe('Data privacy', () => {
+  beforeEach(() => {
+    cy.createSentencePlan().then(planDetails => {
+      cy.openSentencePlan(planDetails.oasysAssessmentPk)
+      cy.visit('/close-anything-not-needed-before-appointment')
+    })
+  })
+
+  it('Data privacy - base', () => {
+    cy.compareSnapshot('base-page')
+  })
+
+  it('Data privacy - error validation', () => {
+    cy.get('button').contains('Confirm').click()
+    cy.compareSnapshot('base-error-validation-page')
+  })
+
+  it('Plan overview - agree to privacy statement and see a given plan', () => {
+    cy.get('#confirm-privacy-checkbox').check()
+    cy.get('button').contains('Confirm').click()
+    cy.compareSnapshot('view-plan-after-agreeing-to-privacy-statement-page')
+  })
+})
