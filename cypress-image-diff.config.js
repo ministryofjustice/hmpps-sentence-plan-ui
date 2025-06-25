acee9813157f396ae1d4d474da2f5238b864eef0
Aiden Herbert <Aiden.Herbert@justice.gov.uk>
2025-06-25 11:17:02 +0100

    Move setting to correct area in cypress-image-diff.config.js

--- cypress-image-diff.config.js
+++ cypress-image-diff.config.js
@@ -1,9 +1,9 @@
 const config = {
   FAILURE_THRESHOLD: 0.05, // 5% difference allowed
+  FAIL_ON_MISSING_BASELINE: true,
   JSON_REPORT: {
     FILENAME: 'cypress-image-diff',
     OVERWRITE: true,
-    FAIL_ON_MISSING_BASELINE: true,
   },
 }
 
