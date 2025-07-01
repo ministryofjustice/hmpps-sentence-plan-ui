f9b3a91aacabc577d6ec1dc443e8ac0975db53fc
Aiden Herbert <Aiden.Herbert@justice.gov.uk>
2025-07-01 16:22:01 +0100

    Clean-up

--- cypress-image-diff-js.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'cypress-image-diff-js' {
-  export function getCompareSnapshotsPlugin(on: any, config: any): void
-  export function addCompareSnapshotCommand(options?: Record<string, any>): void
-}
--- integration_tests/support/commands/snapshot.ts
+++
@@ -1,6 +0,0 @@
-import { compareSnapshotCommand } from 'cypress-image-diff-js'
-
-compareSnapshotCommand({
-  dumpReportJson: true,
-  reportDir: 'cypress-image-diff-html-report',
-})
