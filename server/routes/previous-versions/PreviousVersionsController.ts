cf3b86936c0dee8014a7a0d6aa0e1b4b3c0a61b8
Iana-Bezrodnykh <iana.bezrodnykh@justice.gov.uk>
2025-09-04 16:15:34 +0100

    Add audit event for previous versions list page

--- server/routes/previous-versions/PreviousVersionsController.ts
+++ server/routes/previous-versions/PreviousVersionsController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import { HttpError } from '../../utils/HttpError'
 import locale from './locale.json'
+import { AuditEvent } from '../../services/auditService'
 
 export default class PreviousVersionsController {
   constructor() {}
@@ -9,12 +10,16 @@ export default class PreviousVersionsController {
     const { errors } = req
     try {
       const planUuid = req.services.sessionService.getPlanUUID()
-      const versions = await req.services.assessmentService.getVersionsByUuid(planUuid)
       const pageId = 'previous-versions'
+      const plan = await req.services.planService.getPlanByUuid(planUuid)
+      const versions = await req.services.assessmentService.getVersionsByUuid(planUuid)
+
+      await req.services.auditService.send(AuditEvent.VIEW_PREVIOUS_VERSIONS_PAGE)
 
       return res.render('pages/previous-versions', {
         locale: locale.en,
         data: {
+          planAgreementStatus: plan.agreementStatus,
           pageId,
           oasysReturnUrl: req.services.sessionService.getOasysReturnUrl(),
           versions,
