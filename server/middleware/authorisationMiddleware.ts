bf753f413f13ff9e13485897fffcd68257d83151
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2025-09-16 11:06:34 +0100

    Update authorisationMiddleware

--- server/middleware/authorisationMiddleware.ts
+++ server/middleware/authorisationMiddleware.ts
@@ -6,24 +6,26 @@ import { AccessMode, AuthType } from '../@types/SessionType'
 
 export default function authorisationMiddleware(): RequestHandler {
   return (req, res, next) => {
+    const principalDetails = req.services.sessionService.getPrincipalDetails()
+
+    if (!principalDetails) {
+      req.session.returnTo = req.originalUrl
+      return res.redirect('/sign-in/hmpps-auth')
+    }
+
     // Auth
-    if (req.services.sessionService.getPrincipalDetails()?.authType === AuthType.HMPPS_AUTH) {
+    if (principalDetails.authType === AuthType.HMPPS_AUTH) {
       const { authorities: roles = [] }: JwtPayloadExtended = jwtDecode(req?.user?.token)
 
       if (roles.includes('ROLE_SENTENCE_PLAN')) {
         return next()
       }
+
       throw new HttpError(403, 'No role ROLE_SENTENCE_PLAN found for this user.')
     }
 
     // Handover
-    if (req.services.sessionService.getPrincipalDetails()) {
-      return next()
-    }
-
-    // Failure - no credentials
-    req.session.returnTo = req.originalUrl
-    return res.redirect('/sign-in/hmpps-auth')
+    return next()
   }
 }
 
