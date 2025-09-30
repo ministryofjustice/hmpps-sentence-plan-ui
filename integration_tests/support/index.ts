17680f27fc1a5aa358c0bff17d11d8c7a9a97f13
devkokov <dm.kokov@gmail.com>
2025-09-30 14:34:54 +0100

    ACE-738 Obfuscate dates prior to taking/comparing screenshots

--- integration_tests/support/index.ts
+++ integration_tests/support/index.ts
@@ -33,3 +33,30 @@ Cypress.Commands.add('checkAccessibility', checkAccessibility)
 
 // Feedback
 Cypress.Commands.add('hasFeedbackLink', hasFeedbackLink)
+
+// Obfuscate dynamic data on the page (such as dates) before taking a screenshot for visual regression testing
+Cypress.Screenshot.defaults({
+  onBeforeScreenshot($el) {
+    const replacements = [
+      {
+        find: /([1-9]|[12]\d|3[01]) (January|February|March|April|May|June|July|August|September|October|November|December) \d{4}/g,
+        replace: 'DD MM YYYY',
+      },
+    ]
+
+    const walk = (node: Node) => {
+      // Only process text nodes
+      if (node.nodeType === Node.TEXT_NODE) {
+        replacements.forEach(replacement => {
+          if (replacement.find.test(node.nodeValue)) {
+            // eslint-disable-next-line no-param-reassign
+            node.nodeValue = node.nodeValue.replace(replacement.find, replacement.replace)
+          }
+        })
+      } else {
+        node.childNodes.forEach(walk)
+      }
+    }
+    $el.get().forEach(el => walk(el))
+  },
+})
