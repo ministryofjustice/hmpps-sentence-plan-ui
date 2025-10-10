6301eabb9adc8c5b43f40aff5dac523cce490870
Alex Warhurst <alex.warhurst@digital.justice.gov.uk>
2025-10-10 16:55:07 +0100

    Nav should not show the about link if using hmpps auth

--- integration_tests/e2e/e2e-tests/plan-overview-auth.cy.ts
+++ integration_tests/e2e/e2e-tests/plan-overview-auth.cy.ts
@@ -1,6 +1,13 @@
+import {PlanType} from "../../../server/@types/PlanType";
+import DataGenerator from "../../support/DataGenerator";
+import PlanOverview from "../../pages/plan-overview";
+
 describe('Users with the role can access the plan', () => {
+  const planOverview = new PlanOverview()
+
   beforeEach(() => {
     cy.createSentencePlan().then(planDetails => {
+      cy.wrap(planDetails).as('plan')
       cy.openSentencePlanAuth(planDetails.oasysAssessmentPk, {
         planUuid: planDetails.plan.uuid,
         crn: 'X775086',
@@ -12,5 +19,16 @@ describe('Users with the role can access the plan', () => {
   it('Shows Auth User in the header', () => {
     cy.get('.hmpps-header__account-details__sub-text').should('have.text', 'Auth User')
     cy.checkAccessibility()
+  })
+
+  it('Agreed plan does not show about page when using HMPPS Auth', () => {
+    cy.get<{ plan: PlanType }>('@plan').then(({ plan }) => {
+      cy.addGoalToPlan(plan.uuid, DataGenerator.generateGoal({ title: 'Test Accommodation' })).then(goal => {
+        cy.addStepToGoal(goal.uuid, DataGenerator.generateStep())
+        cy.visit('/plan')
+      })
+    })
+    planOverview.agreePlan()
+    cy.get('.moj-primary-navigation__container').should('not.contain', 'About')
   })
 })
