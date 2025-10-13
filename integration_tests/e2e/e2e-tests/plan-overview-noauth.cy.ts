1ebae3a9d49942399f0de8ab79ad9b224348cc85
NicolaWard-SBG <nicola.ward@justice.gov.uk>
2025-10-13 16:05:24 +0100

    Fix linting and e2e tests

--- integration_tests/e2e/e2e-tests/plan-overview-noauth.cy.ts
+++ integration_tests/e2e/e2e-tests/plan-overview-noauth.cy.ts
@@ -2,15 +2,7 @@ describe('Attempt to access without having logged in', () => {
   it('Shows the 401 error page', () => {
     cy.visit('/plan', { failOnStatusCode: false })
     cy.get('.govuk-heading-l').should('have.text', 'You need to sign in to use this service')
-    cy.contains('a', 'the OASys homepage').should(
-      'have.attr',
-      'href',
-      Cypress.env('OASYS_URL'),
-      )
-    cy.contains('a', 'Manage people on probation').should(
-      'have.attr',
-      'href',
-      Cypress.env('MPOP_URL'),
-    )
+    cy.contains('a', 'the OASys homepage').should('have.attr', 'href', Cypress.env('OASYS_URL'))
+    cy.contains('a', 'Manage people on probation').should('have.attr', 'href', Cypress.env('MPOP_URL'))
   })
 })
