bd8bc3440a1e7065bc0f7a427707df443095b439
NicolaWard-SBG <nicola.ward@justice.gov.uk>
2025-10-13 15:38:54 +0100

    Update sign-in guidance and add Oasys homepage link on 401 page

--- integration_tests/e2e/e2e-tests/plan-overview-noauth.cy.ts
+++ integration_tests/e2e/e2e-tests/plan-overview-noauth.cy.ts
@@ -2,8 +2,12 @@ describe('Attempt to access without having logged in', () => {
   it('Shows the 401 error page', () => {
     cy.visit('/plan', { failOnStatusCode: false })
     cy.get('.govuk-heading-l').should('have.text', 'You need to sign in to use this service')
-    cy.contains('li', 'Go to the OASys homepage to sign-in')
-    cy.contains('a', 'Go to the Manage People on Probation service').should(
+    cy.contains('a', 'the OASys homepage').should(
+      'have.attr',
+      'href',
+      Cypress.env('OASYS_URL'),
+      )
+    cy.contains('a', 'Manage people on probation').should(
       'have.attr',
       'href',
       Cypress.env('MPOP_URL'),
