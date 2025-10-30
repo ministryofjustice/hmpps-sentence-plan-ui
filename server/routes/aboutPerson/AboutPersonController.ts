6e8729351b6c8affc4998d8242bc46cfa9435903
NicolaWard-SBG <nicola.ward@justice.gov.uk>
2025-10-30 11:30:48 +0000

    SP2-1651: Add re-direct and remove dropdown for non-OASys users

--- server/routes/aboutPerson/AboutPersonController.ts
+++ server/routes/aboutPerson/AboutPersonController.ts
@@ -5,6 +5,8 @@ import { formatAssessmentData } from '../../utils/assessmentUtils'
 import { HttpError } from '../../utils/HttpError'
 import { AuditEvent } from '../../services/auditService'
 import { AccessMode } from '../../@types/SessionType'
+import URLS from '../URLs'
+import { AuthType } from '../../@types/SessionType'
 
 export default class AboutPersonController {
   constructor() {}
@@ -12,6 +14,12 @@ export default class AboutPersonController {
   get = async (req: Request, res: Response, next: NextFunction) => {
     let { errors } = req
     try {
+      // if the user is authenticated via HMPPS Auth, redirect to the plan overview page
+      const authType = req.services.sessionService.getPrincipalDetails()?.authType
+      if (authType === AuthType.HMPPS_AUTH){
+        return res.redirect(URLS.PLAN_OVERVIEW)
+      }
+
       const planUuid = req.services.sessionService.getPlanUUID()
       const popData = req.services.sessionService.getSubjectDetails()
       const systemReturnUrl = req.services.sessionService.getSystemReturnUrl()
